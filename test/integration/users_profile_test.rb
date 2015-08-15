e41ef5726c53d6148af80b5c8e25f0835642bf72
Sam Raudabaugh <sraudabaugh@gmail.com>
2015-08-15 09:05:29 -0400

    Add tests for the stats display on user's profile.

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,4 +19,12 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
             assert_match micropost.content, response.body
         end
     end
+
+    test "stats display" do
+        get user_path(@user)
+        assert_select "a[href=?]", following_user_path(@user)
+        assert_select "strong[id=following]", text: "#{@user.following.count.to_s}"
+        assert_select "a[href=?]", followers_user_path(@user)
+        assert_select "strong[id=followers]", text: "#{@user.followers.count.to_s}"
+    end
 end
